bd90268593ee07da8ff44b253146d40d48cce3cd
Alessandro Barbieri <info@alessandrobarbieri.net>
2018-06-06 00:58:49 +0200

    :bug: FIX class not found

--- lib/binda/shopify/installer.rb
+++ lib/binda/shopify/installer.rb
@@ -4,7 +4,7 @@ module Binda
       def create_settings_board values
         raise ArgumentError.new 'You must provide an array or hash of settings values' unless values.is_a?(Array) || values.is_a?(Hash)
         values = Hash[(CONNECTION_KEYS+STRUCTURES.keys).zip(values)] if values.is_a? Array
-        structure = Binda::Structure.find_or_create_by( name: 'Shopify Settings', slug: 'shopify-settings', instance_type: 'board' )
+        structure = ::Binda::Structure.find_or_create_by( name: 'Shopify Settings', slug: 'shopify-settings', instance_type: 'board' )
         board = structure.board
         field_settings = structure.field_groups.first.field_settings
         CONNECTION_KEYS.each do |field_name|
@@ -26,8 +26,8 @@ module Binda
 
       def create_item_structure structure_name, name
         structure_slug = name.parameterize
-        structure = Binda::Structure.find_or_create_by name: name, slug: structure_slug, instance_type: 'component', has_categories: false
-        Binda::Shopify::STRUCTURES[structure_name.to_sym].each do |field_group_slug, fields|
+        structure = ::Binda::Structure.find_or_create_by name: name, slug: structure_slug, instance_type: 'component', has_categories: false
+        ::Binda::Shopify::STRUCTURES[structure_name.to_sym].each do |field_group_slug, fields|
           field_group = structure.field_groups.find_by slug: "#{structure_slug}-#{field_group_slug}"
           if field_group.nil?
             field_group = structure.field_groups.create! name: field_group_slug.titleize, slug: "#{structure_slug}-#{field_group_slug}"
