548440fe859ea805b7e858a1019896a76723624b
Alessandro Barbieri <info@alessandrobarbieri.net>
2018-06-05 22:48:17 +0200

    :recycle: Refactor code following Rubocop suggestion

--- lib/generators/binda/shopify/install_generator.rb
+++ lib/generators/binda/shopify/install_generator.rb
@@ -1,7 +1,7 @@
 module Binda
   module Shopify
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates',__FILE__)
+      source_root File.expand_path('../templates', __FILE__)
       attr_accessor :structure
 
       def start
